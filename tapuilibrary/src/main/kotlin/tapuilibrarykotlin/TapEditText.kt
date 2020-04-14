af12ddb081d91e186dcc81668690e25af6d426fa
AhlaamK-tap <a.kazi@tap.company>
2020-04-14 18:09:04 +0300

    color

--- tapuilibrary/src/main/kotlin/tapuilibrarykotlin/TapEditText.kt
+++ tapuilibrary/src/main/kotlin/tapuilibrarykotlin/TapEditText.kt
@@ -3,7 +3,10 @@ package tapuilibrarykotlin
 import android.content.Context
 import android.graphics.drawable.Drawable
 import android.util.AttributeSet
+import androidx.annotation.ColorInt
+import androidx.annotation.Size
 import androidx.appcompat.widget.AppCompatEditText
+import company.tap.tapuilibrary.R
 
 
 /**
@@ -13,12 +16,36 @@ Copyright (c) 2020    Tap Payments.
 All rights reserved.
  **/
 class TapEditText : AppCompatEditText {
-    constructor(context: Context) : super(context)
-    constructor(context: Context, attrs: AttributeSet?) : super(context, attrs)
-    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
+    @ColorInt
+    private var inputColorInt = 0
+    @Size
+    private var inputTextSize = 0f
+    constructor(context: Context, attrs: AttributeSet?) : super(context, attrs){init(attrs)}
+    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr){init(attrs)}
 
     override fun setError(error: CharSequence, icon: Drawable) {
         setCompoundDrawables(null, null, icon, null)
+    }
+    /**
+     * This init will initilaize the attributes that will be customizable through XML.
+     * InputType is set to string by default.
+     * Color is customizable.
+     * TextSize is Customizable.
+     */
+    private fun init(attrs: AttributeSet?) {
+        if (attrs != null) {
+            val styledAttributes = context.theme.obtainStyledAttributes(
+                attrs,
+                R.styleable.TapEditText,
+                0, 0
+            )
+            try {
+                inputColorInt = styledAttributes.getColor(R.styleable.TapEditText_color, textColors.defaultColor)
+                inputTextSize = styledAttributes.getDimension(R.styleable.TapEditText_fontSize, textSize)
+            } finally {
+                styledAttributes.recycle()
+            }
+        }
     }
 
 }
