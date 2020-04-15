c9fca3a67fca1c6586365944583c8929630d70a8
AhlaamK-tap <a.kazi@tap.company>
2020-04-15 13:03:10 +0300

    add toggle button, tap button

---
+++ tapuilibrary/src/main/kotlin/tapuilibrarykotlin/TapToggle.kt
@@ -0,0 +1,16 @@
+package tapuilibrarykotlin
+
+import android.content.Context
+import android.util.AttributeSet
+import androidx.appcompat.widget.AppCompatToggleButton
+
+/**
+ * Created by AhlaamK on 4/15/20.
+
+Copyright (c) 2020    Tap Payments.
+All rights reserved.
+ **/
+class TapToggle : AppCompatToggleButton {
+    constructor(context: Context, attrs: AttributeSet?) : super(context, attrs)
+    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
+}
