b92264cf757ca2758f7435f1e33ab3d7ad1ba489
Mario Gamal <m.gamal@tap.company>
2020-04-22 09:19:24 +0300

    add theming objects for the views

--- app/src/main/java/company/tap/tapuisample/MainActivity.kt
+++ app/src/main/java/company/tap/tapuisample/MainActivity.kt
@@ -5,26 +5,14 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import company.tap.tapuisample.databinding.ActivityMainBinding
 
-import company.tap.thememanager.ThemeManager
-import kotlinx.android.synthetic.main.activity_main.*
-
 class MainActivity : AppCompatActivity() {
-    var themeManager: ThemeManager? = null
+
     private lateinit var mainBinding: ActivityMainBinding
-    private var currentTheme = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
         mainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-        mainBinding.text = "Testing text"
-        tap_editText.hint = "Please enter details"
-        initAppTheme(R.raw.theme1)
     }
-    private fun initAppTheme(theme: Int) {
-        currentTheme = theme
-        themeManager = ThemeManager()
-        themeManager!!.loadTapTheme(resources, theme)
 
-    }
 }
 
