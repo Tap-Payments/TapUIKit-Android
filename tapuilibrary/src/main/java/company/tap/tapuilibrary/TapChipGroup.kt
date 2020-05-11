11fa0f393b6bfda354695bc6262aa9c095918fe4
Mario Gamal <m.gamal@tap.company>
2020-05-11 11:09:06 +0300

    add chip group

---
+++ tapuilibrary/src/main/java/company/tap/tapuilibrary/TapChipGroup.kt
@@ -0,0 +1,26 @@
+package company.tap.tapuilibrary
+
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.LinearLayout
+import androidx.recyclerview.widget.RecyclerView
+
+/**
+ *
+ * Created by Mario Gamal on 5/8/20
+ * Copyright © 2020 Tap Payments. All rights reserved.
+ *
+ */
+open class TapChipGroup(context: Context?, attrs: AttributeSet?) : LinearLayout(context, attrs) {
+
+    var groupName: TapTextView
+    var groupAction: TapTextView
+    var chipsRecycler: RecyclerView
+
+    init {
+        inflate(context, R.layout.tap_chip_group, this)
+        groupName = findViewById(R.id.group_name)
+        groupAction = findViewById(R.id.group_action)
+        chipsRecycler = findViewById(R.id.chip_recycler)
+    }
+}
