8051588f4a93a9dc997d65c975d51cae04e84289
AhlaamK-tap <a.kazi@tap.company>
2020-06-08 18:15:34 +0300

    Tap textview added

--- tapuilibrary/src/main/java/company/tap/tapuilibrary/TapImageView.kt
+++ tapuilibrary/src/main/java/company/tap/tapuilibrary/TapImageView.kt
@@ -1,9 +1,13 @@
 package company.tap.tapuilibrary
 
 import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Path
+import android.graphics.RectF
 import android.util.AttributeSet
 import androidx.appcompat.widget.AppCompatImageView
 import company.tap.thememanager.theme.ImageViewTheme
+
 
 /**
  * Created by AhlaamK on 4/29/20.
@@ -15,5 +19,15 @@ open class TapImageView(context: Context?, attrs: AttributeSet?) : AppCompatImag
     override fun setTheme(theme: ImageViewTheme) {
         theme.imageResource?.let {setImageResource(R.drawable.tap_logo)  }
         theme.contentDescription?.let { contentDescription = "Tap Logo" }
+    }
+    override fun onDraw(canvas: Canvas) {
+      //  val radius =
+      //      context.resources.getDimension(R.dimen.round_corner_radius)
+        val radius = 8f
+        val path = Path()
+        val rect = RectF(8f, 8f, this.width.toFloat(), this.height.toFloat())
+        path.addRoundRect(rect, radius, radius, Path.Direction.CW)
+        canvas.clipPath(path)
+        super.onDraw(canvas)
     }
 }
