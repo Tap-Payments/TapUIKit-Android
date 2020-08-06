b22ab1544c4f74405d3404870dd0e3d77866fcd0
olamonir <ola.monir7@gmail.com>
2020-08-06 23:35:43 +0300

    ui review

--- app/src/main/java/company/tap/tapuisample/fragments/BottomSheetDialog.kt
+++ app/src/main/java/company/tap/tapuisample/fragments/BottomSheetDialog.kt
@@ -302,10 +302,9 @@ open class BottomSheetDialog : TapBottomSheetDialog(), TapSelectionTabLayoutInte
                 selectedCurrency.text = "SR1000,000.000"
                 itemCount.text = getString(R.string.items)
             } else {
-//                separator.visibility = View.GONE
                 separator_.visibility = View.GONE
                 separatorــ.visibility = View.GONE
-                indicatorSeparator.visibility = View.GONE
+//                indicatorSeparator.visibility = View.GONE
                 childFragmentManager
                     .beginTransaction()
                     .add(R.id.fragment_container1, currencyViewFragment)
