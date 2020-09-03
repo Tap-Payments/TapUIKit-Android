72530e6a4a104e9305949a14503ab730af76eb8c
olamonir <ola.monir7@gmail.com>
2020-09-03 16:36:37 +0300

    - Refactor itemsAdapter

--- tapuilibrary/src/main/java/company/tap/tapuilibrary/uikit/adapters/ItemAdapter.kt
+++ tapuilibrary/src/main/java/company/tap/tapuilibrary/uikit/adapters/ItemAdapter.kt
@@ -18,25 +18,24 @@ import company.tap.tapuilibrary.uikit.atoms.TapSeparatorView
 import company.tap.tapuilibrary.uikit.atoms.TapTextView
 import company.tap.tapuilibrary.uikit.datasource.ItemViewDataSource
 import company.tap.tapuilibrary.uikit.views.TapListItemView
-import kotlin.collections.ArrayList
 
 
 /**
 Copyright (c) 2020    Tap Payments.
 All rights reserved.
  **/
-class ItemAdapter (private val itemList: ArrayList<Int>) :
+class ItemAdapter(private val itemList: ArrayList<Int>) :
     RecyclerView.Adapter<ItemAdapter.ItemHolder>() {
-   private var previousExpandedPosition = -1
-   private var mExpandedPosition = -1
-    private lateinit var descriptionTextView:TapTextView
-    private lateinit var descText:TapTextView
-    private lateinit var itemViewAdapter:TapListItemView
-    private lateinit var itemSeparator:TapSeparatorView
-    private lateinit var discount:TapTextView
-    private lateinit var totalAmount:TapTextView
-    private lateinit var totalQuantity:TapTextView
-    private lateinit var itemName:TapTextView
+    private var previousExpandedPosition = -1
+    private var mExpandedPosition = -1
+    private lateinit var descriptionTextView: TapTextView
+    private lateinit var descText: TapTextView
+    private lateinit var itemViewAdapter: TapListItemView
+    private lateinit var itemSeparator: TapSeparatorView
+    private lateinit var discount: TapTextView
+    private lateinit var totalAmount: TapTextView
+    private lateinit var totalQuantity: TapTextView
+    private lateinit var itemName: TapTextView
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemHolder {
         val v =
@@ -53,113 +52,135 @@ class ItemAdapter (private val itemList: ArrayList<Int>) :
 
     @SuppressLint("SetTextI18n")
     override fun onBindViewHolder(holder: ItemHolder, position: Int) {
-        initView(holder,position)
+        initView(holder, position)
         setTheme()
         setFonts()
-        checkItemListPosition(position)
+//        checkItemListPosition(position)
     }
 
     private fun initView(holder: ItemHolder, position: Int) {
-        descriptionTextView  = holder.itemView.findViewById(R.id.description_textView)
+        descriptionTextView = holder.itemView.findViewById(R.id.description_textView)
         descText = holder.itemView.findViewById(R.id.show_description)
         itemSeparator = holder.itemView.findViewById(R.id.itemseparator)
         totalQuantity = holder.itemView.findViewById(R.id.total_quantity)
-         discount  = holder.itemView.findViewById(R.id.discount_text)
-         totalAmount  = holder.itemView.findViewById(R.id.total_amount)
-         itemName = holder.itemView.findViewById(R.id.item_title)
+        discount = holder.itemView.findViewById(R.id.discount_text)
+        totalAmount = holder.itemView.findViewById(R.id.total_amount)
+        itemName = holder.itemView.findViewById(R.id.item_title)
 
-        val isExpanded = position === mExpandedPosition
-        descriptionTextView.text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex exercitation ullamco laboris nisi ut aliquip ex exercitation ullamco laboris nisi ut aliquip ex exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+        val isExpanded = position == mExpandedPosition
+        descriptionTextView.text =
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex exercitation ullamco laboris nisi ut aliquip ex exercitation ullamco laboris nisi ut aliquip ex exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
         descriptionTextView.visibility = if (isExpanded) View.VISIBLE else View.GONE
         holder.itemView.isActivated = isExpanded
 
         holder.itemView.setOnClickListener {
+            itemViewAdapter.visibility= View.VISIBLE
             mExpandedPosition = if (isExpanded) -1 else position
             notifyItemChanged(previousExpandedPosition)
             notifyItemChanged(position)
         }
 
-        if(isExpanded){
-            previousExpandedPosition = position
-            descText.text = LocalizationManager.getValue("hideDesc","ItemList")
-        }else{
-            descText.text =  LocalizationManager.getValue("showDesc","ItemList")
+        if (isExpanded) {
+//            previousExpandedPosition = position
+            descText.text = LocalizationManager.getValue("hideDesc", "ItemList")
+        } else {
+            descText.text = LocalizationManager.getValue("showDesc", "ItemList")
         }
 
     }
 
-    private fun setFonts(){
+    private fun setFonts() {
         itemName.typeface = Typeface.createFromAsset(
             context?.assets, TapFont.tapFontType(
-                TapFont.RobotoRegular))
+                TapFont.RobotoRegular
+            )
+        )
         totalAmount.typeface = Typeface.createFromAsset(
             context?.assets, TapFont.tapFontType(
-                TapFont.RobotoRegular))
+                TapFont.RobotoRegular
+            )
+        )
         discount.typeface = Typeface.createFromAsset(
             context?.assets, TapFont.tapFontType(
-                TapFont.RobotoLight))
+                TapFont.RobotoLight
+            )
+        )
         descText.typeface = Typeface.createFromAsset(
             context?.assets, TapFont.tapFontType(
-                TapFont.RobotoLight))
+                TapFont.RobotoLight
+            )
+        )
         descriptionTextView.typeface = Typeface.createFromAsset(
             context?.assets, TapFont.tapFontType(
-                TapFont.RobotoLight))
+                TapFont.RobotoLight
+            )
+        )
         itemViewAdapter.setItemViewDataSource(getItemViewDataSource())
-        totalQuantity.typeface=Typeface.createFromAsset(
+        totalQuantity.typeface = Typeface.createFromAsset(
             context?.assets, TapFont.tapFontType(
-                TapFont.RobotoRegular))
+                TapFont.RobotoRegular
+            )
+        )
     }
 
-
-    private fun checkItemListPosition(position: Int){
-        if(itemList[position]%2==0){
-            discount.text = LocalizationManager.getValue("Discount","ItemList")
-            totalAmount.paintFlags =
-                totalAmount.paintFlags or Paint.STRIKE_THRU_TEXT_FLAG
-            itemName.text = "ITEM TITLE "+itemList[position]
-        }else {
-//            discount.visibility= View.INVISIBLE
-            totalAmount.paintFlags =
-                totalAmount.paintFlags and Paint.STRIKE_THRU_TEXT_FLAG.inv()
-            itemName.text = "VERY LOOOONNGGGG ITEM TITLE ITEM TITLE TITLE ITEM TITLETITLE ITEM TITLETITLE ITEM TITLETITLE ITEM TITLETITLE ITEM TITLETITLE ITEM TITLE "+itemList[position]
-        }
-    }
+//    private fun checkItemListPosition(position: Int) {
+//        if (itemList[position] % 2 == 0) {
+//            discount.visibility= View.VISIBLE
+//            discount.text = LocalizationManager.getValue("Discount", "ItemList")
+//            totalAmount.paintFlags = totalAmount.paintFlags or Paint.STRIKE_THRU_TEXT_FLAG
+//            itemName.text = "ITEM TITLE " + itemList[position]
+//        } else {
+////            discount.visibility= View.INVISIBLE
+////            totalAmount.paintFlags = totalAmount.paintFlags and Paint.STRIKE_THRU_TEXT_FLAG.inv()
+//            itemName.text = "VERY LOOOONNGGGG ITEM TITLE ITEM TITLE TITLE ITEM TITLETITLE ITEM TITLETITLE ITEM TITLETITLE ITEM TITLETITLE ITEM TITLETITLE ITEM TITLE " + itemList[position]
+//        }
+//    }
 
 
-    fun setTheme(){
+    fun setTheme() {
 
         itemViewAdapter.setBackgroundColor(Color.parseColor(ThemeManager.getValue("itemsList.backgroundColor")))
         val descriptionTextViewTheme = TextViewTheme()
-        descriptionTextViewTheme.textColor = Color.parseColor(ThemeManager.getValue("itemsList.item.descLabelColor"))
+        descriptionTextViewTheme.textColor =
+            Color.parseColor(ThemeManager.getValue("itemsList.item.descLabelColor"))
         descriptionTextViewTheme.textSize = ThemeManager.getFontSize("itemsList.item.descLabelFont")
         descriptionTextViewTheme.font = ThemeManager.getFontName("itemsList.item.descLabelFont")
         descriptionTextView.setTheme(descriptionTextViewTheme)
         descText.setTheme(descriptionTextViewTheme)
         discount.setTheme(descriptionTextViewTheme)
+        descriptionTextView.setTheme(descriptionTextViewTheme)
 
         val totalQuantityTextViewTheme = TextViewTheme()
-        totalQuantityTextViewTheme.textColor = Color.parseColor(ThemeManager.getValue("itemsList.item.count.countLabelColor"))
-        totalQuantityTextViewTheme.textSize = ThemeManager.getFontSize("itemsList.item.count.countLabelFont")
-        totalQuantityTextViewTheme.font = ThemeManager.getFontName("itemsList.item.count.countLabelFont")
+        totalQuantityTextViewTheme.textColor =
+            Color.parseColor(ThemeManager.getValue("itemsList.backgroundColor"))
+        totalQuantityTextViewTheme.textSize =
+            ThemeManager.getFontSize("itemsList.item.count.countLabelFont")
+        totalQuantityTextViewTheme.font =
+            ThemeManager.getFontName("itemsList.item.count.countLabelFont")
         totalQuantity.setTheme(totalQuantityTextViewTheme)
 
 
         val totalAmountTextViewTheme = TextViewTheme()
-        totalAmountTextViewTheme.textColor = Color.parseColor(ThemeManager.getValue("itemsList.item.calculatedPriceLabelColor"))
-        totalAmountTextViewTheme.textSize = ThemeManager.getFontSize("itemsList.item.calculatedPriceLabelFont")
-        totalAmountTextViewTheme.font = ThemeManager.getFontName("itemsList.item.calculatedPriceLabelFont")
+        totalAmountTextViewTheme.textColor =
+            Color.parseColor(ThemeManager.getValue("itemsList.item.calculatedPriceLabelColor"))
+        totalAmountTextViewTheme.textSize =
+            ThemeManager.getFontSize("itemsList.item.calculatedPriceLabelFont")
+        totalAmountTextViewTheme.font =
+            ThemeManager.getFontName("itemsList.item.calculatedPriceLabelFont")
         totalAmount.setTheme(totalAmountTextViewTheme)
 
 
         val itemTitleTextViewTheme = TextViewTheme()
-        itemTitleTextViewTheme.textColor = Color.parseColor(ThemeManager.getValue("itemsList.item.titleLabelColor"))
+        itemTitleTextViewTheme.textColor =
+            Color.parseColor(ThemeManager.getValue("itemsList.item.titleLabelColor"))
         itemTitleTextViewTheme.textSize = ThemeManager.getFontSize("itemsList.item.titleLabelFont")
         itemTitleTextViewTheme.font = ThemeManager.getFontName("itemsList.item.titleLabelFont")
         itemName.setTheme(itemTitleTextViewTheme)
 
 
         val separatorViewTheme = SeparatorViewTheme()
-        separatorViewTheme.strokeColor = Color.parseColor(ThemeManager.getValue("itemsList.separatorColor"))
+        separatorViewTheme.strokeColor =
+            Color.parseColor(ThemeManager.getValue("itemsList.separatorColor"))
         itemSeparator.setTheme(separatorViewTheme)
 
     }
@@ -171,5 +192,8 @@ class ItemAdapter (private val itemList: ArrayList<Int>) :
             totalQuantity = "2"
         )
     }
+
+
+
 
 }
