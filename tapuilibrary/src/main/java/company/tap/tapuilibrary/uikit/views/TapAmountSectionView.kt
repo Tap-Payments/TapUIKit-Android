28ca2f93408a45533b69f5da5f48aecfb3132884
olamonir <ola.monir7@gmail.com>
2020-09-08 10:54:19 +0300

    - Set Theme to TapPaymentInput - Set Theme to TapSwitch

--- tapuilibrary/src/main/java/company/tap/tapuilibrary/uikit/views/TapAmountSectionView.kt
+++ tapuilibrary/src/main/java/company/tap/tapuilibrary/uikit/views/TapAmountSectionView.kt
@@ -75,7 +75,7 @@ class TapAmountSectionView: LinearLayout {
         itemCount.setTheme(buttonTheme)
         val shapeAppearanceModel = ShapeAppearanceModel()
             .toBuilder()
-            .setAllCorners(CornerFamily.ROUNDED, (ThemeManager.getValue("amountSectionView.itemsNumberButtonBackgroundColor") as Int).toFloat())
+            .setAllCorners(CornerFamily.ROUNDED, (ThemeManager.getValue("amountSectionView.itemsNumberButtonCorner") as Int).toFloat())
             .build()
         val shapeDrawable = MaterialShapeDrawable(shapeAppearanceModel)
         ViewCompat.setBackground(itemCount, shapeDrawable)
