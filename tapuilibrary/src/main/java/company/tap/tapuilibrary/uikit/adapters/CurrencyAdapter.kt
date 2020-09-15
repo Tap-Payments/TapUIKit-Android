f228b269c76156dbf368663c9f9b9faf3f7c1ced
olamonir <ola.monir7@gmail.com>
2020-09-15 19:58:30 +0300

    - fix UI of Currency chip

--- tapuilibrary/src/main/java/company/tap/tapuilibrary/uikit/adapters/CurrencyAdapter.kt
+++ tapuilibrary/src/main/java/company/tap/tapuilibrary/uikit/adapters/CurrencyAdapter.kt
@@ -75,15 +75,15 @@ class CurrencyAdapter(private val photos: ArrayList<CurrencyModel>) :
             tapCard_Chip?.setTheme(chipTheme)
 
 
-            tapCard_Chip?.let {
-                setBorderedView(
-                    it,
-                    (ThemeManager.getValue("amountSectionView.itemsNumberButtonCorner") as Int).toFloat(),
-                    0.0f,
-                   Color.GREEN,
-                    Color.GREEN,
-                    Color.GREEN                )
-            }
+//            tapCard_Chip?.let {
+//                setBorderedView(
+//                    it,
+//                    (ThemeManager.getValue("amountSectionView.itemsNumberButtonCorner") as Int).toFloat(),
+//                    0.0f,
+//                   Color.GREEN,
+//                    Color.GREEN,
+//                    Color.GREEN                )
+//            }
         }
     }
 
@@ -97,26 +97,26 @@ class CurrencyAdapter(private val photos: ArrayList<CurrencyModel>) :
              */
 
 //            holder.itemView.setBackgroundResource(R.drawable.border_currency)
-            tapCard_Chip?.let {
-                setBorderedView(it,
-                    (ThemeManager.getValue("horizontalList.chips.radius")as Int).toFloat(),
-                    7.0f,
-                    Color.parseColor(ThemeManager.getValue("horizontalList.chips.savedCardChip.selected.shadow.color")),
-                    Color.parseColor(ThemeManager.getValue("horizontalList.chips.savedCardChip.backgroundColor")),
-                    Color.parseColor(ThemeManager.getValue("horizontalList.chips.savedCardChip.backgroundColor"))
-                )
-            }
+//            tapCard_Chip?.let {
+//                setBorderedView(it,
+//                    (ThemeManager.getValue("horizontalList.chips.radius")as Int).toFloat(),
+//                    7.0f,
+//                    Color.parseColor(ThemeManager.getValue("horizontalList.chips.savedCardChip.selected.shadow.color")),
+//                    Color.parseColor(ThemeManager.getValue("horizontalList.chips.savedCardChip.backgroundColor")),
+//                    Color.parseColor(ThemeManager.getValue("horizontalList.chips.savedCardChip.backgroundColor"))
+//                )
+//            }
         } else{
-            tapCard_Chip?.let {
-                setBorderedView(
-                    it,
-                    (ThemeManager.getValue("amountSectionView.itemsNumberButtonCorner") as Int).toFloat(),
-                    0.0f,
-                    ThemeManager.getValue("horizontalList.chips.currencyChip.unSelected.shadow.color"),
-                    Color.parseColor(ThemeManager.getValue("horizontalList.chips.currencyChip.backgroundColor")),
-                    Color.parseColor(ThemeManager.getValue("horizontalList.chips.currencyChip.unSelected.shadow.color"))
-                )
-            }
+//            tapCard_Chip?.let {
+//                setBorderedView(
+//                    it,
+//                    (ThemeManager.getValue("amountSectionView.itemsNumberButtonCorner") as Int).toFloat(),
+//                    0.0f,
+//                    ThemeManager.getValue("horizontalList.chips.currencyChip.unSelected.shadow.color"),
+//                    Color.parseColor(ThemeManager.getValue("horizontalList.chips.currencyChip.backgroundColor")),
+//                    Color.parseColor(ThemeManager.getValue("horizontalList.chips.currencyChip.unSelected.shadow.color"))
+//                )
+//            }
         }
 
 //            holder.itemView.setBackgroundResource(R.drawable.border_unclick)
