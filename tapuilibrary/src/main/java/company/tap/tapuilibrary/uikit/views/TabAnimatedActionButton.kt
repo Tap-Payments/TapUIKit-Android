b4a178a0f6f3402182687b6ebbba622c5be2ba04
olamonir <ola.monir7@gmail.com>
2020-09-24 13:49:24 +0300

    - action button data source

--- tapuilibrary/src/main/java/company/tap/tapuilibrary/uikit/views/TabAnimatedActionButton.kt
+++ tapuilibrary/src/main/java/company/tap/tapuilibrary/uikit/views/TabAnimatedActionButton.kt
@@ -14,6 +14,7 @@ import android.widget.TextView
 import androidx.annotation.DrawableRes
 import androidx.cardview.widget.CardView
 import androidx.core.view.setMargins
+import company.tap.taplocalizationkit.LocalizationManager
 import company.tap.tapuilibrary.R
 import company.tap.tapuilibrary.fontskit.enums.TapFont
 import company.tap.tapuilibrary.themekit.ThemeManager
@@ -69,7 +70,7 @@ class TabAnimatedActionButton : CardView, MorphingAnimation.OnAnimationEndListen
 
      fun initActionButtonDataSource(backgroundColor: Int? = null, textColor:Int? = null, buttonText: String? = null ){
         dataSource = ActionButtonDataSource(
-            text = buttonText ?: "Pay",
+            text = buttonText ,
             textSize = 18f,
             textColor = textColor ?: Color.parseColor(ThemeManager.getValue("actionButton.Invalid.titleLabelColor")),
             cornerRadius = 100f,
