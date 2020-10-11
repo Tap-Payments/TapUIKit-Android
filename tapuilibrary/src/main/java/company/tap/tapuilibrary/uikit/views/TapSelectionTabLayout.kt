eda10a6fbe863f7775406867ba568a19a83715b0
olamonir <ola.monir7@gmail.com>
2020-10-11 21:12:36 +0300

    - hide clear view if input text is empty

--- tapuilibrary/src/main/java/company/tap/tapuilibrary/uikit/views/TapSelectionTabLayout.kt
+++ tapuilibrary/src/main/java/company/tap/tapuilibrary/uikit/views/TapSelectionTabLayout.kt
@@ -256,7 +256,7 @@ class TapSelectionTabLayout(context: Context?, attrs: AttributeSet?) :
             override fun onTabReselected(tab: TabLayout.Tab?) {}
             override fun onTabUnselected(tab: TabLayout.Tab?) {}
             override fun onTabSelected(tab: TabLayout.Tab?) {
-//                resetBehaviour()
+                resetBehaviour()
 //                fadeOtherTabs(tab?.position)
                 tabLayoutInterface?.onTabSelected(tab?.position)
             }
