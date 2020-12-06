b25ba0ec58c0c28fd9d0e032a46608d494c43e58
AhlaamK-tap <a.kazi@tap.company>
2020-12-06 11:32:56 +0300

    bug fix

--- tapuilibrary/src/main/java/company/tap/tapuilibrary/uikit/fragment/CurrencyViewFragment.kt
+++ tapuilibrary/src/main/java/company/tap/tapuilibrary/uikit/fragment/CurrencyViewFragment.kt
@@ -14,6 +14,7 @@ import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import company.tap.checkout.internal.dummygener.Currencies1
 
 import company.tap.checkout.internal.dummygener.Items1
 import company.tap.tapuilibrary.R
