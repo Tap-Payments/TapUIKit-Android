4f531b103e3f241a2acbc853779e7595d9d3a8dc
olamonir <ola.monir7@gmail.com>
2020-12-06 21:31:29 +0300

    - Localization for action button

--- tapuilibrary/src/main/java/company/tap/tapuilibrary/uikit/organisms/GoPayLoginInput.kt
+++ tapuilibrary/src/main/java/company/tap/tapuilibrary/uikit/organisms/GoPayLoginInput.kt
@@ -281,13 +281,13 @@ class GoPayLoginInput(context: Context?, attrs: AttributeSet?) :
         loginTabLayout.removeAllTabs()
         loginTabLayout.addTab(
             loginTabLayout.newTab().setCustomView(
-                getThemedTabText(dataSource?.emailTabText ?: "EMAIL", true)
+                getThemedTabText(dataSource?.emailTabText ?: LocalizationManager.getValue("email", "Common"), true)
             )
         )
 
         loginTabLayout.addTab(
             loginTabLayout.newTab().setCustomView(
-                getThemedTabText(dataSource?.phoneTabText ?: "PHONE", false)
+                getThemedTabText(dataSource?.phoneTabText ?: LocalizationManager.getValue("phone", "Common"), false)
             )
         )
 
