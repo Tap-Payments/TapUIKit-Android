c7cd459bcb146466d493fed3eb46393026e31412
olamonir <ola.monir7@gmail.com>
2020-12-28 09:46:05 +0300

    - Blur transparent background

--- tapuilibrary/src/main/java/company/tap/tapuilibrary/uikit/organisms/GoPayPasswordInput.kt
+++ tapuilibrary/src/main/java/company/tap/tapuilibrary/uikit/organisms/GoPayPasswordInput.kt
@@ -198,7 +198,7 @@ class GoPayPasswordInput(context: Context?, attrs: AttributeSet?) :
         passwordTextInput.setErrorColor(Color.parseColor(ThemeManager.getValue("goPay.passwordField.underline.filled.backgroundColor")))
         passwordTextInput.setTextColor(Color.parseColor(ThemeManager.getValue("goPay.passwordField.textColor")))
         passwordTextInput.setHintTextColor(Color.parseColor(ThemeManager.getValue("goPay.passwordField.placeHolderColor")))
-        passwordTextInput.textSize = ThemeManager.getValue("goPay.passwordField.textFont")
+        passwordTextInput.textSize = ThemeManager.getFontSize("goPay.passwordField.textFont").toFloat()
         passwordTextInput.backgroundTintList = ColorStateList.valueOf(Color.parseColor(ThemeManager.getValue("goPay.passwordField.underline.empty.backgroundColor")))
         textInputLayout.backgroundTintList = ColorStateList.valueOf(Color.parseColor(ThemeManager.getValue("goPay.passwordField.underline.empty.backgroundColor")))
         changeEmailCardView.setCardBackgroundColor(Color.parseColor(ThemeManager.getValue("GlobalValues.Colors.whiteTwo")))
