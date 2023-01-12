e2d29db73dbc7bed5e7977088d3aef90991dcf95
AhlaamK-tap <a.kazi@tap.company>
2023-01-12 15:49:07 +0300

    passed theme name in thememanager
    - updated junit lib

--- app/src/main/java/company/tap/tapuisample/activities/MainActivity.kt
+++ app/src/main/java/company/tap/tapuisample/activities/MainActivity.kt
@@ -19,10 +19,7 @@ import android.os.Handler
 import android.security.keystore.KeyGenParameterSpec
 import android.security.keystore.KeyProperties
 import android.util.Log
-import android.view.Menu
-import android.view.MenuItem
-import android.view.View
-import android.view.WindowManager
+import android.view.*
 import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatDelegate
@@ -77,9 +74,9 @@ class MainActivity : BaseActivity(),
         LocalizationManager.loadTapLocale(resources, R.raw.lang)
 //        ThemeManager.loadTapTheme(resources, R.raw.defaultdarktheme, "defaultdarktheme")
 //        if (ThemeManager.currentTheme.isNotEmpty() && ThemeManager.currentTheme.contains("dark"))
-            ThemeManager.loadTapTheme(this,"https://gist.githubusercontent.com/AhlaamK-tap/976650c60952b525d51b739254cd417c/raw/4ca48d48f3cd5dc8a0d4ab49b788bb3e2e9bdc0c/defaultlighttheme1.json")
-         //  ThemeManager.loadTapTheme(this,"https://gist.githubusercontent.com/AhlaamK-tap/2ca0cbeaf430c6d40baa4d0700024848/raw/aba506185d267ee9d379b5c84d71c8cbed1cbbf2/darktheme.json")
-//        else   ThemeManager.loadTapTheme(this,"https://gist.githubusercontent.com/AhlaamK-tap/9862436dff3b3ca222243dad3705ec6a/raw/76f9bd9d4e12e7d9dccf5f4969baf09c15a1ea47/lighttheme")
+            ThemeManager.loadTapTheme(this,"https://gist.githubusercontent.com/AhlaamK-tap/976650c60952b525d51b739254cd417c/raw/4ca48d48f3cd5dc8a0d4ab49b788bb3e2e9bdc0c/defaultlighttheme1.json","lighttheme")
+         //  ThemeManager.loadTapTheme(this,"https://gist.githubusercontent.com/AhlaamK-tap/2ca0cbeaf430c6d40baa4d0700024848/raw/aba506185d267ee9d379b5c84d71c8cbed1cbbf2/darktheme.json","darktheme")
+//        else   ThemeManager.loadTapTheme(this,"https://gist.githubusercontent.com/AhlaamK-tap/9862436dff3b3ca222243dad3705ec6a/raw/76f9bd9d4e12e7d9dccf5f4969baf09c15a1ea47/lighttheme","lighttheme")
 
 
         supportActionBar?.setBackgroundDrawable( ColorDrawable(Color.parseColor("#005959")))
