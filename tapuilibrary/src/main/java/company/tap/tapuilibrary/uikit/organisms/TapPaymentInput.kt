d57168c5b9178a4ec08cb302c3d56b34bc399175
AhlaamK-tap <a.kazi@tap.company>
2023-01-22 13:14:44 +0300

    updated

--- tapuilibrary/src/main/java/company/tap/tapuilibrary/uikit/organisms/TapPaymentInput.kt
+++ tapuilibrary/src/main/java/company/tap/tapuilibrary/uikit/organisms/TapPaymentInput.kt
@@ -75,20 +75,19 @@ class TapPaymentInput(context: Context?, attrs: AttributeSet?) :
       //  cardScannerButton.setImageResource(scannerIcon)
         clearView.setImageResource(closeIcon)
       //  nfcButton.setImageResource(nfcIcon)
-       if (ThemeManager.currentTheme.isNotEmpty() && ThemeManager.currentTheme.contains("dark"))cardInputChipView.setBackgroundResource(
-            R.drawable.border_unclick_black
-        )
-        else  cardInputChipView.setBackgroundResource(R.drawable.border_unclick)
-
+        if (ThemeManager.currentTheme.isNotEmpty() && ThemeManager.currentTheme.contains("dark")) {
+            cardInputChipView.setBackgroundResource(R.drawable.border_unclick_black)
+        } else {
+            cardInputChipView.setBackgroundResource(R.drawable.border_unclick)
+        }
         setBorderedView(
             cardInputChipView,
-            (ThemeManager.getValue("horizontalList.chips.radius") as Int).toFloat(),// corner raduis
+            15.0f,// corner raduis
             0.0f,
-            Color.parseColor(ThemeManager.getValue("horizontalList.chips.goPayChip.selected.shadow.color")),// stroke color
-            Color.parseColor(ThemeManager.getValue("horizontalList.chips.gatewayChip.backgroundColor")),// tint color
-            Color.parseColor(ThemeManager.getValue("horizontalList.chips.goPayChip.unSelected.shadow.color"))
-        )// shadow color
-
+            Color.parseColor(ThemeManager.getValue("horizontalList.chips.currencyChip.unSelected.shadow.color")),
+            Color.parseColor(ThemeManager.getValue("horizontalList.chips.currencyChip.backgroundColor")),
+            Color.parseColor(ThemeManager.getValue("horizontalList.chips.currencyChip.unSelected.shadow.color"))
+        )
     }
     fun setDisplayMetrics(displayMetrics: Int) {
         this.displayMetrics = displayMetrics
